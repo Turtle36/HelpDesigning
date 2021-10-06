55ba6cc2551b9e8d406ae914f9ed546ee3de0b3b
fawwaz <kalkarium@gmail.com>
2021-10-06 14:32:20 +0700

    Change login and sign up button position

--- app/main.py
+++ app/main.py
@@ -11,15 +11,27 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route("/S3Kf028Dk3kdSa3dkdkKFJA488(!839kdk:1!/fkKAKejsja")
+@app.route("/admin", methods=['GET', 'POST'])
 # mengambil header dari objek request
-def headers():
-    headers = request.headers
-    response = ['%s = %s' % (key, value) \
-                for key, value in sorted(headers.items())
-                ]
-    response = '<br/>'.join(response)
-    return response
+def admin():
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == "@boing#admin":
+            if password == "#F@boing1234!!Admin":
+                headers = request.headers
+                response = ['%s = %s' % (key, value) \
+                            for key, value in sorted(headers.items())
+                            ]
+                response = '<br/>'.join(response)
+                return response
+            else:
+                return redirect("/login")
+        else:
+            return redirect("/login")
+
+    return render_template("admin/login.html")
 
 
 from app.views import *
