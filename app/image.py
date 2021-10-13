215450ce3ec183079564213d77ef07dabf9a0ce6
fawwaz <kalkarium@gmail.com>
2021-10-13 10:45:02 +0700

    edit file profile.html

--- app/image.py
+++ app/image.py
@@ -4,10 +4,14 @@ from flask import url_for, redirect
 
 @app.route("/image/icon.png")
 def icon_png():
-    return redirect(url_for("static", filename='img/icon.png'))
+    return redirect(url_for("static", filename='img/png/icon.png'))
 
 
 @app.route("/image/gold.png")
 def gold_png():
-    return redirect(url_for("static", filename='img/gold.png'))
+    return redirect(url_for("static", filename='img/png/gold.png'))
 
+
+@app.route("/image/user.png")
+def user_png():
+    return redirect(url_for("static", filename='img/png/user.png'))
