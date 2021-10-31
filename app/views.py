e795403f1ddcaf2900335597b3e235f20211c176
fawwaz <kalkarium@gmail.com>
2021-11-01 06:47:37 +0700

    solved

--- app/views.py
+++ app/views.py
@@ -3,9 +3,9 @@ from app.main import app
 from app.models import db, article as Article, sign_up as Sign_Up, login as Login, customers as Customers, news as News
 
 
-@app.route("/.well-known/pki-validation/25E1A9227E7D65D587902BCA3A8DF222.txt")
-def A9227E7D65D587902BCA3A8DF222_txt():
-    return render_template("txt/25E1A9227E7D65D587902BCA3A8DF222.txt")
+@app.route("/.well-known/pki-validation/11115E6A2B140B0BC8DBCE0AB90586E1.txt")
+def TXT():
+    return render_template("txt/11115E6A2B140B0BC8DBCE0AB90586E1.txt")
 
 
 @app.route("/delete/article/<name>", methods=['GET', 'POST'])
