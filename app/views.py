cb4625060b91d6d7648ce1fa16249f5bc3074ace
fawwaz <kalkarium@gmail.com>
2021-11-01 07:56:31 +0700

    certificate

--- app/views.py
+++ app/views.py
@@ -3,9 +3,9 @@ from app.main import app
 from app.models import db, article as Article, sign_up as Sign_Up, login as Login, customers as Customers, news as News
 
 
-@app.route("/.well-known/pki-validation/0A6770CDC9D9E3410017F348A8613AF9.txt")
+@app.route("/.well-known/pki-validation/994DE953A1924178C389B1A3E5C0C2F9.txt")
 def TXT():
-    return render_template("txt/0A6770CDC9D9E3410017F348A8613AF9.txt")
+    return render_template("txt/994DE953A1924178C389B1A3E5C0C2F9.txt")
 
 
 @app.route("/delete/article/<name>", methods=['GET', 'POST'])
